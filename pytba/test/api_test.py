81598b2157d81086987ad44c603e68d8ebfff74f
Thing342 <wesjordansemailaddress@gmail.com>
2016-09-20 21:29:32 -0400

    Add more tests

--- pytba/test/api_test.py
+++ pytba/test/api_test.py
@@ -16,6 +16,17 @@ class TestApiMethods(unittest.TestCase):
         with self.assertRaises(TypeError):
             client.tba_get('team/frc2363', app_id='invalid key')
 
+    def test__event_get(self):
+        event = client.event_get('2016tes')
+        self.assertEqual(len(event.teams), 75)
+        self.assertEqual(event.info['name'], 'Tesla Division')
+        self.assertEqual(len(event.matches), 140)
+        self.assertEqual(event.rankings[1][1], '2056')
+
+    def test__team_matches(self):
+        matches = client.team_matches('frc2363', 2016)
+        self.assertEqual(len(matches), 62)
+        self.assertEqual(matches[-1]['opp_score'], 89)
 
 if __name__ == '__main__':
     unittest.main()
